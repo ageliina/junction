2d65ebdfb6165f4785a7c16d5f2b25faca39cdd9
Akke Viitanen <akke.viitanen@helsinki.fi>
2016-11-25 23:47:52 +0200

    Nice plots with combine.

--- combine.py
+++ combine.py
@@ -10,20 +10,68 @@ from sys import argv
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Main
-if __name__=="__main__":
+# Function defs
+"""Read the .csv files fname1 and fname2, return (T, RR)"""
+def read_data(fname1, fname2):
 
-    T_data = np.loadtxt(argv[1])
-    RRs = np.loadtxt(argv[2])
+    T_data = np.loadtxt(fname1)
+    RRs = np.loadtxt(fname2)
 
-    Ts = []
-    Rs = []
+    Ts = np.array([])
+    Rs = np.array([])
 
     for i in range(len(RRs)): 
 
-        Ts.append((T_data[i] + T_data[i+1]) / 2)
-        Rs.append(RRs[i])
+        Ts = np.append(Ts, (T_data[i] + T_data[i+1]) / 2)
+        Rs = np.append(Rs, RRs[i])
+
+    return T_data, Ts, Rs
+
+def get_std(start, stop, Ts, Rs):
+    
+    relevant = (Ts >= start) * (Ts <= stop)
+    return np.mean(Rs[relevant])
 
 
-    plt.plot(Ts, Rs)
+# Main
+if __name__=="__main__":
+
+    T_data, Ts, Rs = read_data(argv[1], argv[2])
+
+    stds = np.array([])
+    t_stds = np.array([])
+    for i in range(7):
+
+        start = i * 1000
+        stop = start + 1000
+        
+        stds = np.append(stds, get_std(start, stop, Ts, Rs))
+        t_stds = np.append(t_stds, (start + stop)/2 )
+
+    mean = np.mean(stds)
+
+    bpms = [] 
+    for R in Rs:
+        bpms.append(60. / R)
+
+    plt.subplot(311)
+
+    plt.title("WHAT IS MY HEART RATE . C O M")
+    plt.plot(Ts, Rs, 'r.', markersize=2.0)
+    ax = plt.gca()
+
+    plt.subplot(312)
+    plt.plot(Ts, bpms, 'b.', markersize=2.0)
+
+    plt.subplot(313, sharex=ax)
+    lo_performance = stds <= mean
+    hi_performance = stds > mean
+    plt.title("PERFORMANCE HISTORY")
+    plt.plot(t_stds[lo_performance], stds[lo_performance], 'r.', markersize=20, label="BAD")
+    plt.plot(t_stds[hi_performance], stds[hi_performance], 'g.', markersize=20, label="GOOD")
+    plt.plot([min(t_stds), max(t_stds)], [mean, mean], 'k--')
+
+    plt.legend()
+
+
     plt.show()
