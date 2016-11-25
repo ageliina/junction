6887b0a915040f6049bc58c4d4b98b2b5d8f45a3
Akke Viitanen <akke.viitanen@helsinki.fi>
2016-11-25 22:52:02 +0200

    Initial commit.

---
+++ combine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+# coding: utf-8
+
+# Author: Akke Viitanen
+
+# Imports
+from sys import argv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Main
+if __name__=="__main__":
+    data1 = np.loadtxt(argv[1])
+    data2 = np.loadtxt(argv[2])
+
+    Ts = []
+    RRs = []
+
+    for i in range(len(data2)): 
+        if i%1000 != 0:
+            continue
+
+        print(i)
+
+        T = (data1[i] + data1[i+1]) / 2
+        RR = data2[i]
+
+        Ts.append(T)
+        RRs.append(RRs)
+
+    plt.plot(Ts, RRs)
+    plt.show()
